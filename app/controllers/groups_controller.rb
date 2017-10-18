25063a50b844e27978d71b0623083ae681bf8b32
岩永規秀 <motohide@iwanagatadashishuus-MacBook-Pro.local>
2017-10-18 20:14:58 +0900

    Edit-rootes,controller
    ルーティングを定義。またコントローラーにnewアクションとcreateアクションを定義。

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,26 @@
+class GroupsController < ApplicationController
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new(group_params)
+    if @group.save
+       redirect_to :root, notice: 'グループを作成しました'
+    else
+       render :new
+    end
+
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  private
+  def group_params
+      params.require(:group).permit(:name, users_id: [])
+  end
+end

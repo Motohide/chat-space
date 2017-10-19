f3b4d910e5b38410e70b969fdb20ccec46fc24ab
岩永規秀 <motohide@iwanagatadashishuus-MacBook-Pro.local>
2017-10-19 12:08:38 +0900

    Edit-migration-file
    マイグレーションファイルに制約を定義。

--- db/migrate/20171018030955_add_name_togroups.rb
+++ db/migrate/20171018030955_add_name_togroups.rb
@@ -1,5 +1,5 @@
 class AddNameTogroups < ActiveRecord::Migration[5.0]
   def change
-    add_column :users, :group_id, :integer
+    add_column :users, :group_id, :integer,null:false, unique: true
   end
 end
--- db/migrate/20171018102825_create_members.rb
+++ db/migrate/20171018102825_create_members.rb
@@ -1,7 +1,10 @@
 class CreateMembers < ActiveRecord::Migration[5.0]
   def change
     create_table :members do |t|
-
+      t.references :user, foreign_key: true, null: false
+      t.references :group, foreign_key: true, null: false
+      t.integer       :user_id
+      t.integer       :group_id
       t.timestamps
     end
   end

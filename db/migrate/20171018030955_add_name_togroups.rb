107b7493835b3b0bc20cf24666a561613c959fc3
岩永規秀 <motohide@iwanagatadashishuus-MacBook-Pro.local>
2017-10-19 13:01:10 +0900

    Edit-migration-file
    マイグレーションファイルを編集。

--- db/migrate/20171018030955_add_name_togroups.rb
+++ db/migrate/20171018030955_add_name_togroups.rb
@@ -1,5 +1,6 @@
 class AddNameTogroups < ActiveRecord::Migration[5.0]
   def change
-    add_column :users, :group_id, :integer,null:false, unique: true
+    add_column :users, :group_id, :integer,
+    t.reference :user, foreign_key: true
   end
 end
--- db/migrate/20171018102825_create_members.rb
+++ db/migrate/20171018102825_create_members.rb
@@ -3,8 +3,6 @@ class CreateMembers < ActiveRecord::Migration[5.0]
     create_table :members do |t|
       t.references :user, foreign_key: true, null: false
       t.references :group, foreign_key: true, null: false
-      t.integer       :user_id
-      t.integer       :group_id
       t.timestamps
     end
   end

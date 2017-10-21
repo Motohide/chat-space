962f15b6b31b5baeb3a2ac1848902eae1cee9865
岩永規秀 <motohide@iwanagatadashishuus-MacBook-Pro.local>
2017-10-21 16:50:15 +0900

    edit-controller
    メッセージ送信機能に必要なアクションをコントローラーに定義。

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,30 @@
 class MessagesController < ApplicationController
-   def index
-     @groups = current_user.groups
-   end
+   before_action :set_group, only:[:index, :create ]
+  def index
+    @groups = current_user.groups
+    @message = Message.new
+  end
+
+  def create
+    @message = Message.new(message_params)
+    @message.user_id = current_user.id
+
+    @groups = current_user.groups
+    if @message.save
+      redirect_to action: :index
+    else
+      p @message.errors
+      flash[:alert] = 'メッセージを入力してください'
+      render :index
+    end
+  end
+
+private
+  def set_group
+    @group = Group.find(params[:group_id])
+  end
+
+  def message_params
+    params.require(:message).permit(:text, :image).merge(group_id: params[:group_id])
+  end
 end

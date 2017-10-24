f88ad350f7ff6dd99f2c0db03e6b059aa3b09c19
岩永規秀 <motohide@iwanagatadashishuus-MacBook-Pro.local>
2017-10-24 19:12:49 +0900

    Edit-controller
    コントローラーに同じ記述があったのでまとめた

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,7 +7,7 @@ class MessagesController < ApplicationController
 
   def create
     @message = current_user.messages.new(message_params)
-    @groups = current_user.groups
+    @groups
     if @message.save
       redirect_to action: :index
     else

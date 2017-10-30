57faa4c0de5903bdaeb235ecabd266b4770ddfe1
岩永規秀 <motohide@iwanagatadashishuus-MacBook-Pro.local>
2017-10-30 17:53:33 +0900

    Add-incremental-serch
    インクリメンタルサーチの導入。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,14 @@
 class UsersController < ApplicationController
   before_action :user_find, only: [:edit, :update]
 
+  def index
+    @users = User.where('username LIKE(?)', "%#{params[:keyword]}%")
+    respond_to do |format|
+      format.html
+      format.json
+    end
+  end
+
   def edit
   end
 
